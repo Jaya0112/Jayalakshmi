f6193a3123d0f5311cde79aca950b6228ae56015
MaGa BuBLy <jayakrishnaveni19@gamil.com>
2022-03-07 11:23:59 +0530

    fifth exercise

---
+++ rubyex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Jayalakshmi A'
+my_age = 22 # not a lie in 2009
+my_height = 52 # inches
+my_weight = 50 # lbs
+my_eyes = 'brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about #{my_name}."
+puts "She's #{my_height} inches tall."
+puts "She's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "She's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
